cf6db67df74e3d5d1460fdced8058997f768d01a
saber2pr <saber2pr@gmail.com>
2019-05-03 11:23:40 +0000

    update

--- src/core/compose.ts
+++ src/core/compose.ts
@@ -1,14 +1,21 @@
 /*
  * @Author: saber2pr
- * @Date: 2019-04-30 12:47:04
+ * @Date: 2019-05-03 11:22:09
  * @Last Modified by: saber2pr
- * @Last Modified time: 2019-05-02 13:31:45
+ * @Last Modified time: 2019-05-03 11:22:37
  */
+import { Context } from './context'
+
 export type Next = () => Promise<any>
 
-export type Job<T = any> = (ctx: T, next: Next) => Promise<void>
+export type Job<T extends Context = Context> = (
+  ctx: T,
+  next: Next
+) => Promise<void>
 
-export const compose = <T>(...jobs: (Job<T>)[]): Job<T> => async (ctx, next) =>
+export const compose = <T extends Context = Context>(
+  ...jobs: (Job<T>)[]
+): Job<T> => async (ctx, next) =>
   jobs.reduceRight(
     (next: Next, job: Job<T>) => async () => await job(ctx, next),
     next
